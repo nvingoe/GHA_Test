1d429fc5cd24160669a88b570df955724cc8b48e
Neil Vingoe <neil.vingoe@fdbhealth.com>
2020-06-04 09:53:09 +0100

    updated to SDK project

--- Engine/Properties/AssemblyInfo.cs
+++ Engine/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Engine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Engine")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Fdb.Rx.AreasOfInterest.Testing.Integration")]
 
 [assembly: InternalsVisibleTo("Fdb.Rx.Platform.Engine")]
--- Results/Properties/AssemblyInfo.cs
+++ Results/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Results")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Results")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Fdb.Rx.AreasOfInterest.Engine")]
 [assembly: InternalsVisibleTo("Fdb.Rx.AreasOfInterest.Testing.Integration")]
 
